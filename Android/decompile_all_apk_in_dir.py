cb48310d18270e7c62b82ee8c894cca78f5eaa33
Omeh2003 <omeh2003@gmail.com>
2020-03-18 19:18:32 +0300

    Скрипт на Python распаковывает с помощью apktool все apk файлы в текущем каталоге. Имя каталога распаковки используется из имени файла. Так же apktool запускается с ключем -b для удобного мержа :)

---
+++ Android/decompile_all_apk_in_dir.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import subprocess
+
+
+def main():
+    os.chdir(os.path.abspath(os.path.curdir))
+    file = os.path.curdir
+    strings = os.listdir(file)
+
+    s = ""
+    for s in strings:
+
+        print(s)
+        print(os.path.isfile(os.path.normpath(s)))
+        if os.path.isfile(s) and s.rpartition(".")[2] == "apk":
+            cmd = f"apktool d -b {os.path.normpath(s)}"
+            subprocess.call(cmd, stderr=None, stdout=None, shell=True)
+    print("Готов: " + s)
+    pass
+
+
+if __name__ == '__main__':
+    main()
